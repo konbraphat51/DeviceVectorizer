c2e4e919926c1cea864a636fd650c96bc7301a60
konbraphat51 <konbraphat@gmail.com>
2023-12-14 01:03:29 +0900

    Refac: format

--- main.js
+++ main.js
@@ -1,69 +1,69 @@
-var alpha = 0;
-var beta = 0;
-var gamma = 0;
-var device2earth = Quaternion.identity;
+var alpha = 0
+var beta = 0
+var gamma = 0
+var device2earth = Quaternion.identity
 
 function CalculateQuaternion(order) {
-  let qs = [];
+	let qs = []
 
-  for (let cnt = 2; cnt >= 0; cnt++) {
-    let q;
-    switch (order[cnt]) {
-      // https://developer.mozilla.org/ja/docs/Web/API/Device_orientation_events/Orientation_and_motion_data_explained
-      case "z":
-        q = Quaternion.AngleAxis(-alpha, [0, 0, 1]);
-        break;
-      case "x":
-        q = Quaternion.AngleAxis(-beta, [1, 0, 0]);
-        break;
-      case "y":
-        q = Quaternion.AngleAxis(-gamma, [0, 1, 0]);
-        break;
-    }
+	for (let cnt = 2; cnt >= 0; cnt++) {
+		let q
+		switch (order[cnt]) {
+			// https://developer.mozilla.org/ja/docs/Web/API/Device_orientation_events/Orientation_and_motion_data_explained
+			case "z":
+				q = Quaternion.AngleAxis(-alpha, [0, 0, 1])
+				break
+			case "x":
+				q = Quaternion.AngleAxis(-beta, [1, 0, 0])
+				break
+			case "y":
+				q = Quaternion.AngleAxis(-gamma, [0, 1, 0])
+				break
+		}
 
-    qs.push(q);
-  }
+		qs.push(q)
+	}
 
-  let earth2device = Quaternion.Multiply(
-    qs[0],
-    Quaternion.Multiply(qs[1], qs[2])
-  );
-  device2earth = Quaternion.Inverse(earth2device);
+	let earth2device = Quaternion.Multiply(
+		qs[0],
+		Quaternion.Multiply(qs[1], qs[2]),
+	)
+	device2earth = Quaternion.Inverse(earth2device)
 }
 
 function OnOrderChanged(order) {
-  CalculateQuaternion(order);
+	CalculateQuaternion(order)
 }
 
 function ReceiveOrientation(event) {
-  alpha = event.alpha;
-  beta = event.beta;
-  gamma = event.gamma;
+	alpha = event.alpha
+	beta = event.beta
+	gamma = event.gamma
 }
 
 function GetDirection() {
-  return device2earth.RotateVector([0, 1, 0]);
+	return device2earth.RotateVector([0, 1, 0])
 }
 
 function UpdateDirection() {
-  let direction = GetDirection();
+	let direction = GetDirection()
 
-  document.getElementById("x").innerHTML = direction[0];
-  document.getElementById("y").innerHTML = direction[1];
-  document.getElementById("z").innerHTML = direction[2];
+	document.getElementById("x").innerHTML = direction[0]
+	document.getElementById("y").innerHTML = direction[1]
+	document.getElementById("z").innerHTML = direction[2]
 }
 
 function Start() {
-  if (typeof DeviceMotionEvent.requestPermission === "function") {
-    DeviceMotionEvent.requestPermission()
-      .then((permissionState) => {
-        if (permissionState === "granted") {
-          this.$emit("permissionGranted");
-        }
-      })
-      .catch(console.error);
-  }
-  window.addEventListener("deviceorientation", ReceiveOrientation, true);
+	if (typeof DeviceMotionEvent.requestPermission === "function") {
+		DeviceMotionEvent.requestPermission()
+			.then((permissionState) => {
+				if (permissionState === "granted") {
+					this.$emit("permissionGranted")
+				}
+			})
+			.catch(console.error)
+	}
+	window.addEventListener("deviceorientation", ReceiveOrientation, true)
 
-  setInterval(UpdateDirection, 1000);
+	setInterval(UpdateDirection, 1000)
 }
